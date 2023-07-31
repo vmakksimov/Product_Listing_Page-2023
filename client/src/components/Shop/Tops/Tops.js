fb0732464364a98f1632d6d21d55ff503527cfa6
Viktor <vmakksimov@gmail.com>
2023-07-31 03:38:24 +0300

    Adding Product Grid

--- client/src/components/Shop/Tops/Tops.js
+++ client/src/components/Shop/Tops/Tops.js
@@ -7,6 +7,11 @@ import top5 from '../../../styles/images/top5.jpg'
 import top6 from '../../../styles/images/top6.jpg'
 import top7 from '../../../styles/images/top7.jpg'
 import top8 from '../../../styles/images/top8.jpg'
+import top10 from '../../../styles/images/top10.jpg'
+import top11 from '../../../styles/images/top11.jpg'
+import top12 from '../../../styles/images/top12.jpg'
+import top13 from '../../../styles/images/top13.jpg'
+
 
 export const Tops = () => {
     return (
@@ -22,73 +27,148 @@ export const Tops = () => {
                             <img src={top1} alt="" />
                         </div>
                         <div className="product-info">
+                            <p>Black top </p>
                             <p>Price: 50$</p>
-                            <p>Color: Black</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
                         </div>
                     </article>
                     <article className="product-item">
                         <div className="product-image">
                             <img src={top2} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
                         <div className="product-image">
-                        <img src={top3} alt="" />
+                            <img src={top3} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
                         <div className="product-image">
-                        <img src={top4} alt="" />
+                            <img src={top4} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                 </section>
                 <section className="items-row-2">
                     <article className="product-item">
                         <div className="product-image">
-                        <img src={top5} alt="" />
+                            <img src={top5} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
                         <div className="product-image">
-                        <img src={top6} alt="" />
+                            <img src={top6} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
                         <div className="product-image">
-                        <img src={top7} alt="" />
+                            <img src={top7} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
                         <div className="product-image">
-                        <img src={top8} alt="" />
+                            <img src={top8} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>Black top </p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                 </section>
                 <section className="items-row-3">
+                <article className="product-item">
+                        <div className="product-image">
+                            <img src={top10} alt="" />
+                        </div>
+                        <div className="product-info">
+                            <p>Bae solid fitted Tee</p>
+                            <p>Stars: </p>
+                            <p>Price: 20$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
+                    </article>
                     <article className="product-item">
                         <div className="product-image">
-                        
+                            <img src={top11} alt="" />
                         </div>
-                        <div className="product-info"></div>
+                        <div className="product-info">
+                            <p>EZwear Solid Drop Shoulder Tee</p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
+                        <div className="product-image">
+                            <img src={top12} alt="" />
+                        </div>
+                        <div className="product-info">
+                            <p>EZwear Solid Crop Tank</p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                     <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
+                        <div className="product-image">
+                            <img src={top13} alt="" />
+                        </div>
+                        <div className="product-info">
+                            <p>BASICS Solid Crop Tube Top</p>
+                            <p>Price: 50$</p>
+                            <p>Price: 50$</p>
+                            <p>Men Cartoon Graphic Tee</p>
+                            <button>Add to cart</button>
+                        </div>
                     </article>
                 </section>
                 <section className="items-row-4">
