9fd0044d9185199d0df24d8ad03da2679d802278
Viktor <vmakksimov@gmail.com>
2023-08-02 21:16:38 +0300

    Adding Sort

--- client/src/components/Shop/Tops/Tops.js
+++ client/src/components/Shop/Tops/Tops.js
@@ -6,12 +6,18 @@ import { Checkbox } from '../Sections'
 export const Tops = ({ products }) => {
 
     const [checked, setChecked] = useState([]);
+    const [colored, setColor] = useState([]);
     const gender = ['male', 'female']
+    const colors = ['Black', 'White', 'Khaki', 'Light Grey', 'Dark Grey', 'Brown']
 
-    const males = products.filter(x => x.gender == 'male')
-    const females = products.filter(x => x.gender == 'female')
+    const genders = products.filter(x => checked.includes(x.gender))
+    const color = products.filter(x => colored.includes(x.color))
+    const allMatch = products.filter(x => colored.includes(x.color) && checked.includes(x.gender))
 
-    const checkHandler = (e) => {
+    console.log(allMatch)
+
+
+    const genderHandler = (e) => {
         let updatedList = [...checked];
 
         if (e.target.checked) {
@@ -23,16 +29,36 @@ export const Tops = ({ products }) => {
         setChecked(updatedList);
     }
 
+    const colorHandler = (e) => {
+        let updatedList = [...colored];
+
+        if (e.target.checked) {
+            updatedList = [...colored, e.target.value];
+        } else {
+            updatedList.splice(colored.indexOf(e.target.value), 1);
+        }
+        setColor(updatedList);
+    }
+
 
     return (
         <section class="tops-content">
             <div class="div1">
-                <div className="checkList">
-                    <div className="title">Your CheckList:</div>
-                    <div className="list-container">
+                <div className="filterList">Filter by:
+                    <div className="title">Gender</div>
+                    <div className="filter-container">
                         {gender.map((item, index) => (
                             <div key={index}>
-                                <input value={item} type="checkbox" onChange={checkHandler} />
+                                <input value={item} type="checkbox" onChange={genderHandler} />
+                                <span>{item}</span>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="title">Color</div>
+                    <div className="filter-container">
+                        {colors.map((item, index) => (
+                            <div key={index}>
+                                <input value={item} type="checkbox" onChange={colorHandler} />
                                 <span>{item}</span>
                             </div>
                         ))}
@@ -40,181 +66,48 @@ export const Tops = ({ products }) => {
                 </div>
             </div>
             <div class="div2">Counter Prodcuts </div>
-            <div class="div3">Category Name & Description </div>
-            <div class="div4">Sort</div>
+            <div class="div3">Category Name & Description</div>
+            <div class="div4">
+                <div class="dropdown">Sort by
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Recommended
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Alphabetical a-z</a></li>
+                        <li><a class="dropdown-item" href="#">Alphabetical z-a</a></li>
+                        <li><a class="dropdown-item" href="#">Lowest price</a></li>
+                        <li><a class="dropdown-item" href="#">Highest price</a></li>
+                    </ul>
+                </div>
+            </div>
             <div class="div5">
-                {/* <section className="items-row-1">
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={tops.image} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top2} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top3} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top4} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                </section>
-                <section className="items-row-2">
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top5} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top6} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top7} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top8} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Black top </p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                </section>
-                <section className="items-row-3">
-                <article className="product-item">
-                        <div className="product-image">
-                            <img src={top10} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>Bae solid fitted Tee</p>
-                            <p>Stars: </p>
-                            <p>Price: 20$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top11} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>EZwear Solid Drop Shoulder Tee</p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top12} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>EZwear Solid Crop Tank</p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image">
-                            <img src={top13} alt="" />
-                        </div>
-                        <div className="product-info">
-                            <p>BASICS Solid Crop Tube Top</p>
-                            <p>Price: 50$</p>
-                            <p>Price: 50$</p>
-                            <p>Men Cartoon Graphic Tee</p>
-                            <button>Add to cart</button>
-                        </div>
-                    </article>
-                </section>
-                <section className="items-row-4">
-                    <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
-                    </article>
-                    <article className="product-item">
-                        <div className="product-image"></div>
-                        <div className="product-info"></div>
-                    </article>
-                </section> */}
-                {checked.length > 0
-                    ? checked == 'male' ? males.map(x => <TopsDetails key={x._id} tops={x} />) : females.map(x => <TopsDetails key={x._id} tops={x} />)
-                    : products.map(x => <TopsDetails key={x._id} tops={x} />)
+
+                {checked.length == 0 && colored.length > 0
+                    ? color.map(x => <TopsDetails key={x._id} tops={x} />)
+                    : allMatch.length > 0
+                        ? allMatch.map(x => <TopsDetails key={x._id} tops={x} />)
+                        : checked.length == 1
+
+                            ? genders.map(x => <TopsDetails key={x._id} tops={x} />)
+                            : products.map(x => <TopsDetails key={x._id} tops={x} />)
                 }
+
+                {/* {allMatch.length > 0
+                ? allMatch.map(x => <TopsDetails key={x._id} tops={x} />)
+                :   checked.length == 1
+
+                ? genders.map(x => <TopsDetails key={x._id} tops={x} />)
+                : products.map(x => <TopsDetails key={x._id} tops={x} />)
+            } */}
+
+                {/* {checked.length == 1
+                    ? genders.map(x => <TopsDetails key={x._id} tops={x} />)
+                    : products.map(x => <TopsDetails key={x._id} tops={x} />)
+                } */}
+                {/* {colored.length > 0 &&
+                     color.map(x => <TopsDetails key={x._id} tops={x} />)
+                 
+                } */}
             </div>
             <div class="div6">Load More </div>
         </section>
